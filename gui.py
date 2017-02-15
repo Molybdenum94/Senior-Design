efcbfd4a51e74a590ef14a862a92af2b3604c243
CamIHelpYou <cdl3022@gmail.com>
2017-02-15 15:05:34 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -25,31 +25,31 @@ frame3 = tkinter.Frame(mainwindow)
 frame3.pack()
 
 # ***** Widgets *****
-title = tkinter.Label(topFrame, text="Welcome to the ATE/Verilog Translator")
+title = tkinter.Label(topFrame, bg='white', text="Welcome to the ATE/Verilog Translator")
 title.pack()
 photo = tkinter.PhotoImage(file="images.gif")
-L0 = tkinter.Label(topFrame, image=photo)
+L0 = tkinter.Label(topFrame, image=photo, bg='white')
 L0.pack()
-L1 = tkinter.Label(frame1, text="Input File Path  ", padx=2)
+L1 = tkinter.Label(frame1, text="Input File Path  ", padx=2, bg='white')
 L1.pack(side=tkinter.LEFT)
 def getinput():
     mainwindow.inputfileName = filedialog.askopenfilename( filetypes = (("ATE files", "*.cpp"), ("Verilog files", "*.vams")))
     if mainwindow.inputfileName != '':
         inputtext.set(mainwindow.inputfileName)
 inputtext = tkinter.StringVar()
-B1 = tkinter.Button(frame1, command=getinput, relief=tkinter.SUNKEN, textvariable=inputtext)
+B1 = tkinter.Button(frame1, command=getinput, relief=tkinter.SUNKEN, textvariable=inputtext, bg='white')
 inputtext.set("                                         ")
 B1.pack(side=tkinter.LEFT)
-spacer = tkinter.Label(space, text="           ")
+spacer = tkinter.Label(space, text="           ", bg='white')
 spacer.pack()
-L2 = tkinter.Label(frame2, text="Output File Path")
+L2 = tkinter.Label(frame2, text="Output File Path", bg='white')
 L2.pack(side=tkinter.LEFT)
 def getoutput():
     mainwindow.outputfilePath = filedialog.askdirectory()
     if mainwindow.outputfilePath != '':
         outputtext.set(mainwindow.outputfilePath)
 outputtext = tkinter.StringVar()
-B2 = tkinter.Button(frame2, command=getoutput, relief=tkinter.SUNKEN, textvariable=outputtext)
+B2 = tkinter.Button(frame2, command=getoutput, relief=tkinter.SUNKEN, textvariable=outputtext, bg='white')
 outputtext.set("                                         ")
 B2.pack(side=tkinter.LEFT)
 def helpmenu():
@@ -59,21 +59,26 @@ def helpmenu():
     menu.configure(background='white')
     inputtitle = tkinter.Label(menu,
                                text="Input File Path:",
-                               font="Helvetica 16 bold").pack()
+                               font="Helvetica 16 bold",
+                               bg='white').pack()
     inputhelp1 = tkinter.Label(menu,
                                text="1. Click the box to the right of 'Input File Path' to open file explorer\n2. Select the .cpp or .vams file you want to convert\n3. Click 'Open'. The file path chosen will be displayed.",
                                justify=tkinter.LEFT,
-                               font="Helvetica 12").pack()
+                               font="Helvetica 12",
+                               bg='white').pack()
     spacer = tkinter.Label(menu,
-                           text=" ").pack()
+                           text=" ",
+                           bg='white').pack()
     outputtitle = tkinter.Label(menu,
                                 text="Output File Path:",
-                                font="Helvetica 16 bold").pack()
+                                font="Helvetica 16 bold",
+                                bg='white').pack()
     outputhelp1 = tkinter.Label(menu,
                                 text="1. Click the box to the right of 'Output File Path' to open file explorer\n2. Select the DIRECTORY you want the output file to be in\n3 Click 'Choose'. The file path chosen will be displayed.",
                                 justify=tkinter.LEFT,
-                                font="Helvetica 12").pack()
-helpbutton = tkinter.Button(frame3, text="Help", pady=15, command=helpmenu)
+                                font="Helvetica 12",
+                                bg='white').pack()
+helpbutton = tkinter.Button(frame3, text="Help", pady=15, command=helpmenu, bg='white')
 helpbutton.pack()
 
 # ***** Main Function *****
@@ -81,11 +86,10 @@ def convert():
     button.destroy()
     button2 = tkinter.Button(frame3, text="Converting...", state=tkinter.DISABLED)
     button2.pack()
-    time.sleep(3)
 
     #Put Code Here
         
-button = tkinter.Button(frame3, text="Convert", command=convert)
+button = tkinter.Button(frame3, text="Convert", command=convert, bg='white')
 button.pack()
 
 mainwindow.mainloop()
