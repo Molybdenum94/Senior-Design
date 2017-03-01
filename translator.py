d19910320bdb5f69651b023a290e596b6098d3c0
MY-BAE\Pirat <dsiu@utexas.edu>
2017-03-01 16:19:04 -0600

    translator + GUI integration

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 import cpp_vams_library as lb
 import cpp_vams_parser as p
-
+import tkinter
 import sys, getopt, os.path
 
 # Driver prototype for C++/Verilog translator
@@ -17,62 +17,16 @@ Verilog_to_Cpp = {
 	"real":"double",
 }
 
-def main(argv): #command line input-checker
-    inputfile = ''
-    outputfile = ''
-    results =[]
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print ("")
-        print ("command.py -i <inputfile> -o <outputfile>")
-        print ("")
-        sys.exit()
-    for opt, arg in opts:
-        if opt == '-h':
-            print ("")
-            print ("command.py -i <inputfile> -o <outputfilename>")
-            print ("#input and output must be .cpp or .vams; both cannot be the same")
-            print ("")
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-            if os.path.exists(inputfile) == False:
-                print ("")
-                print ("input file not found")
-                print ("")
-                sys.exit()
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-            
-# checks the files to make sure they are opposite types
-    if ".cpp" in inputfile:
-        if ".vams" in outputfile:
-            print ("")
-            print ("input file is in C++ and output file is Verilog to be called:", outputfile)
-            print ("")
-            return inputfile, ".cpp", outputfile
-    elif ".vams" in inputfile:
-        if ".cpp" in outputfile:
-            print ("")
-            print ("input file is in Verilog and output file is in C++ to be called:", outputfile)
-            print ("")
-            return inputfile, ".vams", outputfile
-    print ("")
-    print ("command.py -i <inputfile> -o <outputfilename>")
-    print ("#input and output must be .cpp or .vams; both cannot be the same")
-    print ("")
-    sys.exit()
-    
-if __name__ == "__main__":
-	inputfile, inputtype, outputfile = main(sys.argv[1:])
-	parser = p.Parser(inputfile, inputtype)
+def main(i, o, outputName): #command line input-checker
+	inputPath = i.get()
+        outputPath = o.get()
+	parser = p.Parser(inputPath, inputtype)
 	functions = parser.getFunctions()
 	variables = parser.getVariables()
 	statements = parser.getStatements()
-	if(inputtype == ".cpp"): #translate .cpp to .vams
+	if ".cpp" in inputPath: #translate .cpp to .vams
 		with open(outputfile, 'w') as f:
-			f.write("module " + outputfile.rstrip('.vams') + ';\n')
+			f.write("module " + outputName.rstrip('.vams') + ';\n')
 			for function in functions: #write function definitions
 				f.write(lb.find_Verilog(function) + '\n')
 			for variable in variables: #write variable declarations
